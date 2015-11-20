8b7169ffe40340e548557705cff5ba324bddf674
AnliHuer <AnliHuer@outlook.com>
2015-11-20 18:23:49 +0800

    extract the getListString() about data tranfer to string

--- js/main.js
+++ js/main.js
@@ -3,46 +3,36 @@ function createTime(value) {
   var year = dateTime.getFullYear();
   var month = dateTime.getMonth();
   var date = dateTime.getDate();
-
   month = (month + 1 < 10) ? '0' + (month + 1) : (month + 1).toString();
   date = (date + 1 < 10) ? '0' + (date + 1) : (date + 1).toString();
-
   return year + "-" + month + "-" + date;
+}
+
+
+function getListString(data){
+    var searchList = $('.search-list');
+    var list = '';
+    data.forEach(function(val){
+      list += "<li class = \'bookmark-content\'>" + val.title + "</li> <li class = \'bookmark-datetime\'>Create@" + createTime(val.created) + "</li>" + "<hr>";
+    });
+    searchList.html(list);
 }
 
 
 $(function() {
   var searchInput = $('.search-bar');
-  var searchList = $('.search-list');
-
   $.getJSON('data/bookmark.json',function(data){
-    var allList = '';
-    data.forEach(function(val){
-      allList += "<li class = \'bookmark-content\'>" + val.title + "</li> <li class = \'bookmark-datetime\'>Create@" + createTime(val.created) + "</li>" + "<hr>";
-    });
-    searchList.html(allList);
+    getListString(data);
   });
-
   searchInput.on('keyup', function() {
     var word = searchInput.val();
-
     $.getJSON('data/bookmark.json', function(data) {
       var regExp = new RegExp(word, 'gim');
       var newData = data.filter(function(val) {
         return val.title.search(regExp) > 0;
       });
-
-      var list = '';
-      newData.forEach(function(item) {
-        list += "<li class = \'bookmark-content\'>" + item.title + "</li> <li class = \'bookmark-datetime\'>Create@" + createTime(item.created) + "</li>" + "<hr>";
-      });
-      searchList.html(list);
+      getListString(newData);
     });
 
   });
-
-
-
-
-
 });
